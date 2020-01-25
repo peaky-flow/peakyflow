548942782d8190d49a9a85dffcdfb2e73bf53fcf
fredkingham <fredkingham@gmail.com>
2020-01-25 17:29:32 +0000

    Anything added into the text input is written to the file system.

--- components/PeakFlowFields.js
+++ components/PeakFlowFields.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
 import { View, TextInput, Text, Alert, Button } from 'react-native';
 import { render } from 'react-dom';
+import * as FileSystem from 'expo-file-system';
 
 export default class PeakFlowFields extends Component {
   constructor(props) {
     super(props);
-    this.value;
+    this.value = null;
   }
-  storeValue(value){
-    this.value = value;
+  setValue(value){
+    this.value = value
+  }
+  storeValue(){
+    let path = FileSystem.documentDirectory + "/results.csv"
+    FileSystem.writeAsStringAsync(path, this.value).then((success) => {
+      // do nothing
+    })
+    .catch((err) => {
+      Alert.alert(err.message);
+    })
   }
   render() {
     return (
       <View>
       <TextInput
         style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-        onChangeText={text => this.storeValue(text)}
+        onChangeText={text => this.setValue(text)}
         value={this.value}
       />
         <Button
           title="Press me"
-          onPress={() => Alert.alert(this.value)}
+          onPress={this.storeValue.bind(this)}
         />
       </View>
     );
