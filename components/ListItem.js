14f07553cc052f1fd3803ac84c0367449f579cb9
fredkingham <fredkingham@gmail.com>
2020-01-26 13:26:08 +0000

    Works and does peak flow and work done

--- components/ListItem.js
+++ components/ListItem.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 import { View, TextInput, Text, Alert, Button, FlatList } from 'react-native';
 import styles from './styles'
 
-export default function ListItem({ title }) {
+export default function ListItem({ pefr, workDay }) {
+  let wd;
+  if (workDay) {
+    wd = "Work day"
+  } else {
+    wd = ""
+  }
+
   return (
     <View style={styles.item}>
-      <Text>{title}</Text>
+      <Text>{pefr} {wd}</Text>
+
     </View>
   );
 }
--- components/PeakFlowFields.js
+++ components/PeakFlowFields.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, TextInput, Text, Alert, Button, FlatList, StyleSheet } from 'react-native';
+import { View, TextInput, Text, CheckBox, Alert, Button, FlatList, StyleSheet } from 'react-native';
 import ListItem from './ListItem';
 import styles from './styles'
 import { render } from 'react-dom';
@@ -11,18 +11,18 @@ export default class PeakFlowFields extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: null,
+      pefr: null,
+      workDay: false,
       listValues: []
     }
     this.path = FileSystem.documentDirectory + "/results.csv"
   }
   setValue(value){
-    this.setState({value: value})
+    this.setState({pefr: value})
 
   }
   componentDidMount(){
     FileSystem.readAsStringAsync(this.path).then((success) => {
-      console.log("saving " + success);
       let saved = null;
       if(!success){
         saved = [];
@@ -42,9 +42,10 @@ export default class PeakFlowFields extends Component {
   }
   storeValue(){
     var listValues = this.state.listValues;
-    listValues.push(this.state.value)
+    listValues.unshift({
+      pefr: this.state.pefr, workDay: this.state.workDay
+    })
     this.setState({listValues: listValues, value: null});
-    console.log("setting " + this.state.listValues);
     var listValues = JSON.stringify(this.state.listValues);
       FileSystem.writeAsStringAsync(this.path, listValues).then((success) => {
     })
@@ -55,27 +56,36 @@ export default class PeakFlowFields extends Component {
   render() {
     return (
       <View>
-        <View style={{ height: 400 }}>
+      <TextInput
+        style={{ height: 60, marginBottom: 20, borderColor: 'gray', borderWidth: 1, padding: 20 }}
+        onChangeText={text => this.setValue(text)}
+        keyboardType='numeric'
+        value={this.state.value}
+      />
+      <View style={{ flexDirection: 'row', justifyContent: "center"}}>
+      <CheckBox
+      value={this.state.checked}
+      onValueChange={() => this.setState({ workDay: !this.state.workDay })}
+      />
+      <Text style={{ marginTop: 5}}>Work day</Text>
+      </View>
+      <Button
+          title="Save"
+          style={{ height: 60, marginBottom: 120 }}
+          onPress={this.storeValue.bind(this)}
+        />
+        <View style={{ height: 400, marginTop: 20 }}>
           <FlatList
             style={styles.container}
             data={this.state.listValues}
             renderItem={({ item }) => (
               <ListItem
-                title={item}
+                pefr={item.pefr}
+                workDay={item.workDay}
               />
             )}
             keyExtractor={(item, index) => index.toString()}
           /></View>
-      <TextInput
-        style={{ height: 60, marginBottom: 20, borderColor: 'gray', borderWidth: 1 }}
-        onChangeText={text => this.setValue(text)}
-        value={this.state.value}
-      />
-        <Button
-          title="Press me"
-          style={{ height: 60, marginBottom: 120 }}
-          onPress={this.storeValue.bind(this)}
-        />
       </View>
     );
   }
