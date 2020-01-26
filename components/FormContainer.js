e5fb33615fe2c5dd818c79258542e634ee800c44
Iain Crossingham <irc91914@gmail.com>
2020-01-26 10:26:04 +0000

    First draft for send email

--- components/FormContainer.js
+++ components/FormContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import PeakFlowFields from './PeakFlowFields';
+import SendData from './SendData';
 
 export default class FormContainer extends React.Component {
   render() {
@@ -8,6 +9,7 @@ export default class FormContainer extends React.Component {
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text>Hello, silly!</Text>
         <PeakFlowFields></PeakFlowFields>
+        <SendData></SendData>
       </View>
     );
   }
---
+++ components/SendData.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { View, TextInput, Text, Alert, Button } from 'react-native';
+import { render } from 'react-dom';
+import RNSmtpMailer from "react-native-smtp-mailer";
+
+export default class SendData extends Component {
+  constructor(props) {
+    super(props);
+    this.value;
+  }
+
+  doSendMail(pwd, csv){
+    RNSmtpMailer.sendMail({
+      mailhost: "smtp.gmail.com",
+      port: "465",
+      ssl: true, 
+      username: "peakyflow1@gmail.com",
+      password: pwd,
+      from: "peakyflow1@gmail.com",
+      recipients: "peakyflow1@gmail.com",
+      subject: "Peak flow data",
+      htmlBody: "<h1>Peak flow data</h1><p>Hello</p>",
+      attachmentPaths: [
+        RNFS.DocumentDirectoryPath + "/pefr.csv"
+      ],
+      attachmentNames: [
+        "pefr.csv"
+      ], 
+      attachmentTypes: ["csv"] //needed for android, in ios-only application, leave it empty: attachmentTypes:[]. Generally every img(either jpg, png, jpeg or whatever) file should have "img", and every other file should have its corresponding type.
+    })
+      .then(success => console.log(success))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <View>
+        <TextInput
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          value={this.value}
+        />
+        <Button
+          title="Send data"
+          onPress={() => this.doSendMail(this.value)}
+        />
+      </View>
+    );
+  }
+}
